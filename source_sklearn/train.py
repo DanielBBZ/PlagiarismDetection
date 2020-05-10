223d8bdd62702cd060659f3cc20ec457de3f005f
csltzhou <csltzhou@penfed.org>
2020-05-10 15:00:54 -0400

    update notebook and train.py

--- source_sklearn/train.py
+++ source_sklearn/train.py
@@ -3,11 +3,12 @@ from __future__ import print_function
 import argparse
 import os
 import pandas as pd
+# from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.externals import joblib
 
 ## TODO: Import any additional libraries you need to define a model
-
+from sklearn.svm import LinearSVC
 
 # Provided model load function
 def model_fn(model_dir):
@@ -39,6 +40,8 @@ if __name__ == '__main__':
     parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
     
     ## TODO: Add any additional arguments that you will need to pass into your model
+#     parser.add_argument('--n_estimators', type=int, default=50)
+#     parser.add_argument('--max_depth', type=int, default=4)    
     
     # args holds all passed-in arguments
     args = parser.parse_args()
@@ -56,11 +59,11 @@ if __name__ == '__main__':
     
 
     ## TODO: Define a model 
-    model = None
-    
+    #model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth,random_state=0)
+    model = LinearSVC()
     
     ## TODO: Train the model
-    
+    model.fit(train_x, train_y)
     
     
     ## --- End of your code  --- ##
